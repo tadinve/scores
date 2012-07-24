4a5576396c49fa17c0fa22c51d9219ebe239b023
Eshwar Chandra <eshwarcc@gmail.com>
2012-07-25 00:37:32 +0530

    Changes in datepicker js

--- app/assets/javascripts/datepicker_options.js
+++ app/assets/javascripts/datepicker_options.js
@@ -1,19 +1,11 @@
 $(function() {
-
-	$('.datepicker').datepicker({
-		changeYear: true,
-		yearRange: "-100:c",
-		// dateFormat: "yy-mm-dd",
-		showOn: 'both',
-		buttonImageOnly: true,
-	});
-
+	
 	$.datepicker.setDefaults({
 		changeYear: true,
 		yearRange: "-100:c",
 		// dateFormat: "yy-mm-dd",
-		showOn: 'both',
-		buttonImageOnly: true,
+		// showOn: 'both',
+		// buttonImageOnly: true,
 	});
 
 });
